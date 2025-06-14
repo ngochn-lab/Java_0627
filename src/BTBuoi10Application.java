9c92df7965196d9bb58b114fbb5ee63ee0b5fad0
ngochn-lab <ngochn2461@gmail.com>
2025-06-14 17:50:17 +0700

    Upload BT PhanSo

--- src/BTBuoi10Application.java
+++ src/BTBuoi10Application.java
@@ -1,5 +1,15 @@
 public class BTBuoi10Application {
     public static void main(String[] args) {
+        printDivider(1);
+        PhanSo ps = new PhanSo();
+        ps.nhap();
+        ps.xuat();
 
+        printDivider(2);
+
+    }
+
+    public static void printDivider(int soBai){
+        System.out.println("------------------- BÀI TẬP " + soBai + " ------------------------");
     }
 }
--- src/PhanSo.java
+++ src/PhanSo.java
@@ -1,8 +1,46 @@
+import java.util.Scanner;
+
 public class PhanSo {
     private double tuSo;
     private double mauSo;
 
-    public PhanSo(double tuSo, double mauSo){
+    public PhanSo() {}
 
+    public PhanSo(double tuSo, double mauSo){
+        this.tuSo = tuSo;
+        this.mauSo = mauSo;
+    }
+
+    public void nhap(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Xin mời nhập Tử Số:");
+        tuSo = scanner.nextDouble();
+        System.out.println("Xin mời nhập Mẫu Số:");
+        mauSo = scanner.nextDouble();
+    }
+
+    public void xuat() {
+        System.out.println("Từ số: " + tuSo);
+        System.out.println("Mẫu số: " + mauSo);
+    }
+
+    public void cong(PhanSo phanSo) {
+        double ketQua = (this.tuSo * phanSo.mauSo + this.mauSo * phanSo.tuSo)/(this.mauSo * phanSo.mauSo);
+        System.out.println("Tổng 2 phân số: " + ketQua);
+    }
+
+    public void tru(PhanSo phanSo) {
+        double ketQua = (this.tuSo * phanSo.mauSo - this.mauSo * phanSo.tuSo)/(this.mauSo * phanSo.mauSo);
+        System.out.println("Hiệu 2 phân số: " + ketQua);
+    }
+
+    public void nhan(PhanSo phanSo) {
+        double ketQua = (this.tuSo * phanSo.tuSo)/(this.mauSo * phanSo.mauSo);
+        System.out.println("Tích 2 phân số: " + ketQua);
+    }
+
+    public void chia(PhanSo phanSo) {
+        double ketQua = (this.tuSo * phanSo.mauSo)/(this.mauSo * phanSo.tuSo);
+        System.out.println("Thương 2 phân số: " + ketQua);
     }
 }
